d13a736a28c46695c6e4aa7b61bc4de23ef7f7e4
emrocode <92260849+emrocode@users.noreply.github.com>
2026-06-03 11:05:40 -0300

    chore(ci): upgrade Node and config

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.ts$": ["ts-jest", {
-      useESM: true
-    }],
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
